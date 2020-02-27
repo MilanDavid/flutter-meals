7095610d040684b0350e209b1b2381d5d6ad83be
Milan Jovanovic <miland.sm@gmail.com>
2020-02-27 15:44:31 +0100

    added tabs

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_meals/screens/categories_screen.dart';
 import 'package:flutter_meals/screens/category_meals_screen.dart';
 import 'package:flutter_meals/screens/meal_detail_screen.dart';
+import 'package:flutter_meals/screens/tabs_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: CategoriesScreen(),
+      home: TabsScreen(),
       routes: {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),

47ee31eeb05ff66934ec2b1a12c7bdb0c928bb5a
Milan Jovanovic <miland.sm@gmail.com>
2020-02-28 16:13:40 +0100

    adding drawer, splash and icons

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -6,7 +6,7 @@ class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridView(
-      padding: const EdgeInsets.all(25),
+      padding: const EdgeInsets.all(10),
       children: DUMMY_CATEGORIES
           .map((catData) => CategoryItem(
                 catData.id,
@@ -17,8 +17,8 @@ class CategoriesScreen extends StatelessWidget {
       gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
         maxCrossAxisExtent: 200,
         childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
       ),
     );
   }
